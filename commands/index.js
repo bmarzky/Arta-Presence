71745ea0c1e334f578e18219c3571ed74d8d2705
Bimaaaaaa <bmarzky.official@gmail.com>
2025-12-28 18:45:38 +0700

    Update : flow edit

--- commands/index.js
+++ commands/index.js
@@ -9,7 +9,7 @@ const approveUser = require('./approve/approveUser');
 const approveAtasan = require('./approve/approveAtasan');
 const { sendTyping } = require('../utils/sendTyping');
 const handleLembur = require('./absensi/lembur'); 
-const handleEdit = require('../commands/absensi/editAbsen');
+const handleEdit = require('./absensi/editAbsen');
 
 const ttdFolder = path.join(__dirname, '../assets/ttd/');
 
